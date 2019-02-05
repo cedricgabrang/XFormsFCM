32692a835ef41e24d0b2b54ad7dac2abfbfc2bc3
Cedric <cedric.gabrang@hotmail.com>
2019-02-05 18:22:08 +0800

    Add project files.

---
+++ XFormsFCM/XFormsFCM.Android/MainActivity.cs
@@ -0,0 +1,51 @@
+﻿
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Firebase;
+using System.Collections.Generic;
+
+namespace XFormsFCM.Droid
+{
+    [Activity(Label = "XFormsFCM", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        static readonly string TAG = "MainActivity";
+
+        internal static readonly string CHANNEL_ID = "my_notification_channel";
+        internal static readonly int NOTIFICATION_ID = 100;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
+            FirebaseOptions options = new FirebaseOptions.Builder()
+              .SetApplicationId("1:1063764371053:android:20f3fc5a11498166")
+              .SetApiKey("AIzaSyB3QHD5XSUZenzjaJLBeULbgOItts1IUK0")
+              .SetGcmSenderId("1063764371053")
+              .Build();
+
+            bool hasBeenInitialized = false;
+            IList<FirebaseApp> firebaseApps = FirebaseApp.GetApps(Application.Context);
+            foreach (FirebaseApp app in firebaseApps)
+            {
+                if (app.Name.Equals(FirebaseApp.DefaultAppName))
+                {
+                    hasBeenInitialized = true;
+                    FirebaseApp firebaseApp = app;
+                }
+            }
+
+            if (!hasBeenInitialized)
+            {
+                FirebaseApp firebaseApp = FirebaseApp.InitializeApp(Application.Context, options);
+            }
+
+            LoadApplication(new App());
+        }
+
+    }
+}
---
+++ XFormsFCM/XFormsFCM.Android/MyFirebaseMessagingService.cs
@@ -0,0 +1,44 @@
+﻿using Android.App;
+using Android.Content;
+using Firebase.Messaging;
+using Android.Support.V4.App;
+using Android.Util;
+using XFormsFCM.Droid;
+using System.Collections.Generic;
+
+namespace FCMClient
+{
+    [Service]
+    [IntentFilter(new[] { "com.google.firebase.MESSAGING_EVENT" })]
+    public class MyFirebaseMessagingService : FirebaseMessagingService
+    {
+        const string TAG = "MyFirebaseMsgService";
+        public override void OnMessageReceived(RemoteMessage message)
+        {
+            Log.Debug(TAG, "From: " + message.From);
+            var body = message.GetNotification().Body;
+            Log.Debug(TAG, "Notification Message Body: " + body);
+            SendNotification(body);
+        }
+
+        void SendNotification(string messageBody)
+        {
+            var intent = new Intent(this, typeof(MainActivity));
+            intent.AddFlags(ActivityFlags.ClearTop);
+            
+
+            var pendingIntent = PendingIntent.GetActivity(this,0,intent,
+                                                          PendingIntentFlags.OneShot);
+
+            var notificationBuilder = new NotificationCompat.Builder(this)
+                                      .SetSmallIcon(Resource.Drawable.ic_notification)
+                                      .SetContentTitle("FCM Message")
+                                      .SetContentText(messageBody)
+                                      .SetAutoCancel(true)
+                                      .SetContentIntent(pendingIntent);
+
+            var notificationManager = NotificationManagerCompat.From(this);
+            notificationManager.Notify(0, notificationBuilder.Build());
+        }
+    }
+}
